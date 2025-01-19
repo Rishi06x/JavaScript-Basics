648e9ee76ff3dee91051e658ed6157e8cf77dd9d
Rishi06x <rishiranjith1234@gmail.com>
2025-01-19 21:13:13 +0530

    Created Variables and Datatypes

---
+++ Variables and Datatypes/Datatypes.js
@@ -0,0 +1,6 @@
+let name = "Rishi"; // string
+let age = 18; // number
+let isStudent = false; // boolean
+let city; // undefined
+let salary = null; //Null
+let skills = ['HTML', 'CSS', 'JavaScript']; // Array
---
+++ Variables and Datatypes/TypeConversion.js
@@ -0,0 +1,19 @@
+let name = "123";
+let num = Number(name);
+console.log(num); //output : 123
+console.log(typeof num); //output : number
+
+let age = 25;
+let str = String(age);
+console.log(str); //output : "25"
+console.log(typeof str); //output : String
+
+let bool = true;
+let number = Number(bool);
+console.log(number); //output : 1
+console.log(typeof number); //output : number
+
+let arr = [1, 2, 3];
+let x = String(arr);
+console.log(x); //output : 1,2,3
+console.log(typeof x); //output : string
