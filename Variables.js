17eceaa5acc662e549e4da681cba103a359a8c5b
Rishi06x <rishiranjith1234@gmail.com>
2025-01-19 20:53:28 +0530

    Created Variables.js file

---
+++ Variables.js
@@ -0,0 +1,14 @@
+//Var
+var x = "Javascript"; //var can be updated and re-declared
+var x = "Java"
+console.log(x);
+
+//let 
+let y = "Hello world"; //let can be updated but not re-declared
+y = 'Welcome';
+console.log(y);
+
+//const
+const z = "Apple"; //const cannot be updated and re-declared
+// z = "Banana"; Uncaught TypeError: Assignment to constant variable.
+console.log(z);
