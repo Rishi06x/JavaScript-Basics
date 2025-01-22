5921e19211e0518b3313e72a09635f9b94870b6f
Rishi06x <rishiranjith1234@gmail.com>
2025-01-22 19:21:21 +0530

    Created Conditional Statements

---
+++ Conditional-Statements/elseif.js
@@ -0,0 +1,9 @@
+let age = 24;
+
+if(age <= 12){
+    console.log("You are a child.");
+} else if(age > 12 && age <= 19){
+    console.log("You are a teenager.");
+} else {
+    console.log("You are an adult.");
+}
