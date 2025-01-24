8a42b76b739273a525ce0775fc6b7a76da33c5a6
Rishi06x <rishiranjith1234@gmail.com>
2025-01-24 22:23:33 +0530

    Created Functions

---
+++ Functions/Anonymous.js
@@ -0,0 +1,12 @@
+const name = "Rishi";
+
+const greet = function() {
+    console.log(`Hello, ${name}!`);
+}   
+
+greet();
+
+/*
+Output:
+    Hello, Rishi!
+*/
---
+++ Functions/function.js
@@ -0,0 +1,14 @@
+const name = "Rishi";
+
+// Function to display greeting
+
+function greet() {
+    console.log(`Hello, ${name}!`);
+}
+
+greet(); // Call the function
+
+/*
+Output:
+    Hello, Rishi!
+*/
