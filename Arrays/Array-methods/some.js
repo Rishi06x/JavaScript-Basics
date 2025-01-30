ac2c2757c5798716be1d7e6cd2cb9dd7ffd252ca
Rishi06x <rishiranjith1234@gmail.com>
2025-01-30 21:47:16 +0530

    Created

---
+++ Arrays/Array-methods/some.js
@@ -0,0 +1,7 @@
+const array = [10,20,30,40,50];
+
+const result = array.some(x => x > 25);
+
+console.log(result); //returns true if the array contains some elements that satisfy the condition.
+
+// Output: true
