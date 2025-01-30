22b48b40cfd8d74869e1a6f6b8652b3fc73f5b62
Rishi06x <rishiranjith1234@gmail.com>
2025-01-30 21:46:13 +0530

    Created

---
+++ Arrays/Array-methods/findIndex.js
@@ -0,0 +1,8 @@
+const array = [10,20,30,40,50];
+
+const result = array.findIndex(x => x > 25);
+
+console.log(result); //returns the index of the 1st element that satisfies the condition.
+
+
+// Output: 2
