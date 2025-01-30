223ec72f6757dc585aafb8bcd02486fc60e712d0
Rishi06x <rishiranjith1234@gmail.com>
2025-01-30 21:42:30 +0530

    Created

---
+++ Arrays/Array-methods/every.js
@@ -0,0 +1,7 @@
+const array = [10,20,30,40,50];
+
+const result = array.every(x => x > 25);
+
+console.log(result); //return true if the array contains every element that satisfies the condition.
+
+//Output: false
