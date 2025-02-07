02771278a9b8cc8f59d883be307035363573bdab
Rishi.06 <149419223+Rishi06x@users.noreply.github.com>
2025-02-07 23:58:16 +0530

    Create setTimeout.js

---
+++ setTimeout.js
@@ -0,0 +1,14 @@
+console.log("Start");
+
+setTimeout(() => {
+    console.log("Hello after 2 seconds");
+}, 2000); // Executes after 2000ms (2 seconds)
+
+console.log("End");
+
+/*
+Output:
+    Start
+    End
+    Hello after 2 seconds
+*/
